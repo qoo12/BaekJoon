c44e674bd2bbd8359481590ff5ed90d483858251
qoo12 <121751817+qoo12@users.noreply.github.com>
2023-03-13 03:06:40 +0900

    not_complete_time_out

---
+++ BaekJoon1181.py
@@ -0,0 +1,36 @@
+def del_duplicate(arr) :
+    for i in range(len(arr)) :
+        store = arr[i]
+        arr.remove(arr[i])
+        arr.append(store)
+    
+def len_sorting(arr) :
+    for i in range(len(arr)-1) :
+        j = i+1
+        while len(arr[j-1]) > len(arr[j]) and j > 0 :
+            arr[j-1], arr[j] = arr[j], arr[j-1]
+            j -= 1
+
+def dict_sorting(arr) :
+    i = 0
+    j = 1
+    while i < len(arr) :
+        while len(arr[i]) == j :
+            b = i+1
+            while arr[b-1] > arr[b] and b > 0 :
+                arr[b-1], arr[b] = arr[b], arr[b-1]
+                b -= 1
+            j += 1
+
+input_cnt = int(input())
+sorting_list = []
+
+for i in range(input_cnt) :
+    sorting_list.append(input())
+
+del_duplicate(sorting_list)
+len_sorting(sorting_list)
+dict_sorting(sorting_list)
+
+for i in range(len(sorting_list)) :
+    print(sorting_list[i])
